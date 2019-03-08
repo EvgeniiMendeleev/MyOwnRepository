2195761f7d3d8a2e2aa8180c0b3288185593bbad
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-03-08 14:18:01 +0300

    Update server.c

--- server.c
+++ server.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <stdint.h>
 
+//-------------Состояния--------------------
+#define STOP -2
+#define WaitingOfConnection -1
+#define WaitingOfTurn 0
+#define Turn 1
+//------------------------------------------
+
+//---------Буфер памяти для координат-------
 struct Position{
 
 	float x, y;
 };
+//------------------------------------------
 
 int main()
 {
+	//-----------Создание слушащего сокета---------------------
+
 	int MasterSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	struct sockaddr_in MasSocket;
@@ -20,21 +34,54 @@ int main()
 	bind(MasterSocket,(struct sockaddr*) (&MasSocket), sizeof(MasSocket));
 	
 	listen(MasterSocket, 1);
-	
-	while(1)
-	{
-		int FriendSocket = accept(MasterSocket, 0, 0);
-		//EnemySocket = accept(MasterSocket, 0, 0);
+	//---------------------------------------------------------
 
+	//-----------Ожидание соединения клиентов------------------
+
+	int FirstPlayer = accept(MasterSocket, 0, 0);
+	
+	if(FirstPlayer > 0){
+		
+		printf("First player connected\n");
+		int_least8_t state = WaitingOfConnection;
+		send(FirstPlayer, &state, sizeof(state), MSG_NOSIGNAL);
+	}
+
+	int SecondPlayer = accept(MasterSocket, 0, 0);
+
+	if(SecondPlayer > 0){
+	
+		printf("Second player connected!\n");
+		uint_least8_t stateFirstPlayer = Turn;
+		uint_least8_t stateSecondPlayer = WaitingOfTurn;
+		send(FirstPlayer, &stateFirstPlayer, sizeof(stateFirstPlayer), MSG_NOSIGNAL);
+		send(SecondPlayer, &stateSecondPlayer, sizeof(stateSecondPlayer), MSG_NOSIGNAL);
+	}
+
+	//---------------------------------------------------------
+	
+	//---------------------Процесс обработки данных---------------
+	printf("The game have begun!\n");
+
+	sleep(5);
+
+	while(1){
+		
 		struct Position Buffer;
 
-		recv(FriendSocket, &Buffer, sizeof(Buffer), MSG_NOSIGNAL);
-		printf("Enemy position: %f %f", Buffer.x, Buffer.y);
-	
-		printf("Yes\n");
-		//send(EnemySocket, PositionOfEnemy, MSG_NOSIGNAL);
+		recv(FirstPlayer, &Buffer, sizeof(Buffer), MSG_NOSIGNAL);
 		
+		printf("\nEnemy position: %f %f", Buffer.x, Buffer.y);
+		
+		send(SecondPlayer, &Buffer, sizeof(Buffer), MSG_NOSIGNAL);
+		
+		//--------Теперь ход другого игрока--------
+		int t = FirstPlayer;
+		FirstPlayer = SecondPlayer;
+		SecondPlayer = t;
+		//-----------------------------------------
 	}
+	//------------------------------------------------------------
 
 	return 0;
 }
