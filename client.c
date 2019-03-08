63d3bc42c93eeb5b65098ea9b47877bcd7026b83
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-03-08 14:15:36 +0300

    Update client.c

--- client.c
+++ client.c
@@ -3,33 +3,89 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+//----------Состояния клиента-------
+#define Stop -2
+#define WaitingOfConnection -1
+#define WaitingOfTurn 0
+#define Turn 1
+//----------------------------------
+
+//-----------Координаты-------------
 struct Position{
 
 	float x, y;
 };
+//----------------------------------
 
 int main(){
 
-	while(1){
+	//----------------------Создание клиентского сокета и подключение клиента------------------
+	int_least8_t state; //Текущее состояние клиента
 
-		struct Position Buffer;
-		printf("Enter coordinates of cell");
+	int SendSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-		scanf(" %f %f", &Buffer.x, &Buffer.y);
-		int SendSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		
-		struct sockaddr_in SSocket;
+	struct sockaddr_in SSocket;
 
-		SSocket.sin_family = AF_INET;
-		SSocket.sin_port = htons(25567);
-		inet_pton(AF_INET, "127.0.0.1", &(SSocket.sin_addr));
+	SSocket.sin_family = AF_INET;
+	SSocket.sin_port = htons(25567);
+	inet_pton(AF_INET, "10.10.1.227", &(SSocket.sin_addr));
 
-		connect(SendSocket, (struct sockaddr*) (&SSocket), sizeof(SSocket));
-		send(SendSocket, &Buffer, sizeof(Buffer), MSG_NOSIGNAL);	
-		shutdown(SendSocket, SHUT_RDWR);
-		//close(SendSocket);
+	if(connect(SendSocket, (struct sockaddr*) (&SSocket), sizeof(SSocket)) == -1){
+
+		printf("error\n");
+		return 0;
 	}
+	//-----------------------------------------------------------------------------------------
+
+	//---------------Ожидание второго игрока------------------
+	printf("Waiting of second player\n");
+	
+	//Читаем состояние клиента.
+	//Если мы будем ожидать подключение, то значит это был первый игрок,
+	//иначе это был второй игрок, и мы идём дальше, давая второму игроку
+	//состояние ОЖИДАНИЕ ХОДА ПРОТИВНИКА на сервере.
+	recv(SendSocket, &state, sizeof(state), MSG_NOSIGNAL);
+
+	printf("%d\n", state);
+
+	while(state == WaitingOfConnection){
+	
+		recv(SendSocket, &state, sizeof(state), MSG_NOSIGNAL);
+	}
+	//--------------------------------------------------------
+	
+	//------------------------Процесс игры-------------------------
+	printf("The game have begun\n");
+
+	sleep(5);
+
+	while(1){
+		struct Position Buffer;
+
+		system("clear");
+
+		if(state == Turn){
+
+			printf("\nEnter coordinates of cell: ");
+			scanf("%f %f", &Buffer.x, &Buffer.y);
+			send(SendSocket, &Buffer, sizeof(Buffer), MSG_NOSIGNAL);
+			state = WaitingOfTurn;
+		}
+		else if(state == WaitingOfTurn){
+
+			printf("Waiting\n");
+			recv(SendSocket, &Buffer, sizeof(Buffer), MSG_NOSIGNAL);
+			printf("Shot in coordinates: %f %f", Buffer.x, Buffer.y);
+			state = Turn;
+		}
+	}
+	//-------------------------------------------------------------
+
+	shutdown(SendSocket, SHUT_RDWR);
 
 	return 0;
 }
