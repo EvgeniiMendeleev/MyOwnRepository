6ff5b03c2bd1a739a7e9eb734c93874d8b2d56fa
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-04-10 19:46:04 +0300

    Create battle.cpp

---
+++ Sea_Battle_qt/battle.cpp
@@ -0,0 +1,16 @@
+//---Библиотеки для работы с сокетами Беркли-
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+//-------------------------------------------
+
+void Preparing_for_Battle(int ClientSocket)
+{
+    
+}
+
+void Battle(int ClientSocket)
+{
+    
+}
