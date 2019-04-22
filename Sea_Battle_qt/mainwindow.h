07f9795d1e9eba1e4f0feea095b0a40b9dc5c1f9
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-04-22 18:16:01 +0300

    Update mainwindow.h

--- Sea_Battle_qt/mainwindow.h
+++ Sea_Battle_qt/mainwindow.h
@@ -2,6 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QKeyEvent>
+#include <QEvent>
+#include <QPixmap>
+#include <QDebug>
+
+//--Библиотеки для работы с сокетами Беркли--
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+//-------------------------------------------
+#include <ship.h>
 
 namespace Ui {
 class MainWindow;
@@ -14,12 +28,24 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+     void Preparing_for_Battle(); //Функция для отображения фрейма с нераставленными кораблями и полем
 
 private slots:
-    void on_Connection_clicked();
+    void on_Connection_clicked();                //Кнопка подключения в главном меню.
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow *ui;     //Главное меню.
+
+    int ClientSocket;       //Сокет клиента, с помощью которого будет общение с сервером.
+
+    //---Переменные для отображения какого - либо фрейма---
+    QPixmap BattleField;
+    QGraphicsScene* scene;
+    //-----------------------------------------------------
+
+    QPoint PosMouse;        //Координаты мыши на экране.
+
+    int* tmpbuffer;         //Временный буфер для поля с кораблями.
 };
 
 #endif // MAINWINDOW_H
