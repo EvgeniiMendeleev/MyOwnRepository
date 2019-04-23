8af5f0a60df24b1cb075b02cfed2146c370ea2a7
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-04-23 19:50:06 +0300

    Update ship.cpp

--- Sea_Battle_qt/ship.cpp
+++ Sea_Battle_qt/ship.cpp
@@ -1,12 +1,38 @@
 #include "ship.h"
 #include <QDebug>
-int width = 125;
-int height = 42;
 
-Ship::Ship(QObject* parent):QObject(parent), QGraphicsItem()
+Ship::Ship(int TypeOfShip, QObject* parent):QObject(parent), QGraphicsItem()
 {
-    TextureOfShip.load(":/img/4ship.jpg");
-    TextureOfShip = TextureOfShip.scaled(width, height, Qt::KeepAspectRatio);
+    switch(TypeOfShip)
+    {
+        case 1:
+            width = 31;
+
+            TextureOfShip.load(":/img/1ship.png");
+            TextureOfShip = TextureOfShip.scaled(width, height, Qt::KeepAspectRatio);
+            break;
+
+        case 2:
+            width = 62;
+
+            TextureOfShip.load(":/img/2ship.png");
+            TextureOfShip = TextureOfShip.scaled(width, height, Qt::KeepAspectRatio);
+            break;
+
+        case 3:
+            width = 93;
+
+            TextureOfShip.load(":/img/3ship.png");
+            TextureOfShip = TextureOfShip.scaled(width, height, Qt::KeepAspectRatio);
+            break;
+
+        case 4:
+            width = 124;
+
+            TextureOfShip.load(":/img/4ship.jpg");
+            TextureOfShip = TextureOfShip.scaled(width, height, Qt::KeepAspectRatio);
+            break;
+    }
 }
 
 Ship::~Ship()
@@ -16,12 +42,12 @@ Ship::~Ship()
 
 QRectF Ship::boundingRect() const
 {
-    return QRectF (50, 50, width, height);
+    return QRectF (x, y, width, height);
 }
 
 void Ship::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
 {
-    painter->drawPixmap(50, 50, TextureOfShip, 0, 0, width, height);
+    painter->drawPixmap(x, y, TextureOfShip, 0, 0, width, height);
     Q_UNUSED(option);
     Q_UNUSED(widget);
 }
@@ -29,7 +55,7 @@ void Ship::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWid
 void Ship::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
 {
     qDebug() << this->pos().x() << this->pos().y() << "\n";
-    this->setPos(mapToScene(QPointF(event->pos().x() - 50, event->pos().y() - 50)));
+    this->setPos(mapToScene(QPointF(event->pos().x() - x, event->pos().y() - y)));
 }
 
 void Ship::mousePressEvent(QGraphicsSceneMouseEvent* event)
@@ -44,3 +70,37 @@ void Ship::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
     Q_UNUSED(event);
 }
 
+void Ship::mouseDoubleClickEvent(QGraphicsSceneMouseEvent* event)
+{
+    //Задел на будущее.
+    if(this->isHorisontal)
+    {
+        setRotation(90);
+        this->isHorisontal = false;
+    }
+    else
+    {
+        setRotation(0);
+        this->isHorisontal = true;
+    }
+}
+
+void Ship::set_x(int x)
+{
+    this->x = x;
+}
+
+void Ship::set_y(int y)
+{
+    this->y = y;
+}
+
+int Ship::get_x()
+{
+    return this->x;
+}
+
+int Ship::get_y()
+{
+    return this->y;
+}
