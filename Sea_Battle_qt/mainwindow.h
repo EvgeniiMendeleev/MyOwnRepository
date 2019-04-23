d4a59b998a1af1c219e739d2a460d474a6d8a150
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-04-23 19:48:36 +0300

    Update mainwindow.h

--- Sea_Battle_qt/mainwindow.h
+++ Sea_Battle_qt/mainwindow.h
@@ -8,6 +8,7 @@
 #include <QEvent>
 #include <QPixmap>
 #include <QDebug>
+#include <QVector>
 
 //--Библиотеки для работы с сокетами Беркли--
 #include <sys/socket.h>
@@ -29,21 +30,25 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
      void Preparing_for_Battle(); //Функция для отображения фрейма с нераставленными кораблями и полем
-
+     void Main_Menu_off();        //Функция для скрытия кадра главного меню
 private slots:
     void on_Connection_clicked();                //Кнопка подключения в главном меню.
 
 private:
     Ui::MainWindow *ui;     //Главное меню.
-
     int ClientSocket;       //Сокет клиента, с помощью которого будет общение с сервером.
 
     //---Переменные для отображения какого - либо фрейма---
-    QPixmap BattleField;
+    QPixmap frame;
     QGraphicsScene* scene;
     //-----------------------------------------------------
 
     QPoint PosMouse;        //Координаты мыши на экране.
+    QVector<Ship*> MyShips; //Вектор всех кораблей (всего их 10)
+    //i = 0 - четырёхпалубник
+    //i = 1,2 - трёхпалубники
+    //i = 3,5 - двухпалубники
+    //i = 6,9 - однопалубники
 
     int* tmpbuffer;         //Временный буфер для поля с кораблями.
 };
