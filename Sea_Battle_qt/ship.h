efdb4b8a34b348a738f3f4bd66c873e63b51526b
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-04-23 19:50:43 +0300

    Update ship.h

--- Sea_Battle_qt/ship.h
+++ Sea_Battle_qt/ship.h
@@ -11,7 +11,11 @@ class Ship: public QObject, public QGraphicsItem
 {
     Q_OBJECT
 public:
-    explicit Ship(QObject* parent = 0);
+    explicit Ship(int TypeOfShip, QObject* parent = 0);
+    void set_x(int x);
+    void set_y(int y);
+    int get_x();
+    int get_y();
     ~Ship();
 signals:
 
@@ -21,8 +25,15 @@ private:
     void mouseMoveEvent(QGraphicsSceneMouseEvent* event);
     void mousePressEvent(QGraphicsSceneMouseEvent* event);
     void mouseReleaseEvent(QGraphicsSceneMouseEvent* event);
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent* event);
 
-    QPixmap TextureOfShip;
+    QPixmap TextureOfShip;      //Текстура корабля.
+    int width;                  //Ширина текстуры корабля.
+    int height = 42;            //Высота текстуры корабля.
+    int x, y;                   //Координаты появления корабля.
+    bool isHorisontal = true;   //Переменная, отвечающая за состояние корабля в пространстве:
+                                //горизонтален он или вертикален.
+
 public slots:
 
 };
