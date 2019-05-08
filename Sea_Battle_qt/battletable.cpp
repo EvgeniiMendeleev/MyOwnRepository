58ec16aa4fbc878b246d87039f7da73ea82f336c
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-05-08 21:42:24 +0300

    Update battletable.cpp

--- Sea_Battle_qt/battletable.cpp
+++ Sea_Battle_qt/battletable.cpp
@@ -1,44 +1,12 @@
 #include "battletable.h"
 
-BattleTable::BattleTable(int Client, QGraphicsScene* scene, QObject *parent) : QObject(parent)
+BattleTable::BattleTable(QObject *parent) : QObject(parent)
 {
-    BUNG.load(":/img/красный-крестик.png");
-    BUNG = BUNG.scaled(24, 24);
-
-    NotBUNG.load(":/img/BlackDot.png");
-    NotBUNG = NotBUNG.scaled(24, 24);
-
-    ClientSocket = Client;
-    Scene = scene;
 }
 
 void BattleTable::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-    Position* InfoForServer = new Position;
-
-    InfoForServer->i = event->pos().y() / 24;
-    InfoForServer->j = event->pos().x() / 24;
-
-    send(ClientSocket, InfoForServer, sizeof(Position), MSG_NOSIGNAL);
-
-    int* isBung = new int;
-    recv(ClientSocket, isBung, sizeof(int), MSG_NOSIGNAL);
-
-    qDebug() << "isBung = " << *isBung << "\n";
-
-    if((*isBung) > 0)
-    {
-        QGraphicsPixmapItem* IsBung = Scene->addPixmap(BUNG);
-        IsBung->setPos(322 + 24 * InfoForServer->j, 102 + 24 * InfoForServer->i);
-    }
-    else if((*isBung) == 0)
-    {
-        QGraphicsPixmapItem* NotIsBung = Scene->addPixmap(NotBUNG);
-        NotIsBung->setPos(322 + 24 * InfoForServer->j, 102 + 24 * InfoForServer->i);
-    }
-
-    delete isBung;
-    delete InfoForServer;
+   emit fire(event->pos().x() / 24, event->pos().y() / 24);
 }
 
 QRectF BattleTable::boundingRect() const
