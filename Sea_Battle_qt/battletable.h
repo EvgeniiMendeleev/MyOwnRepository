8193a9f59342f57da4eb264977be58f404c5a5cf
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-05-08 21:43:03 +0300

    Update battletable.h

--- Sea_Battle_qt/battletable.h
+++ Sea_Battle_qt/battletable.h
@@ -3,42 +3,24 @@
 
 #include <QGraphicsItem>
 #include <QGraphicsSceneMouseEvent>
-#include <QDebug>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <stdint.h>
-
-//--Библиотеки для работы с сокетами Беркли--
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-//-------------------------------------------
-
-struct Position
-{
-    int i, j;
-};
 
 class BattleTable : public QObject, public QGraphicsItem
 {
     Q_OBJECT
 public:
-    explicit BattleTable(int Client, QGraphicsScene* scene, QObject *parent = 0);
+    explicit BattleTable(QObject *parent = 0);
 
 private:
     QRectF boundingRect() const;
     void mousePressEvent(QGraphicsSceneMouseEvent* event);
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
 
-    QPixmap BUNG;
-    QPixmap NotBUNG;
-    int ClientSocket;
-    QGraphicsScene* Scene;
-
 signals:
+    void fire(unsigned short x, unsigned short y);
 
 public slots:
+
+private slots:
 };
 
 #endif // BATTLETABLE_H
