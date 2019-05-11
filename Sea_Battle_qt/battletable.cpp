4f34a1100d2775f6587cb42eda009c537532f187
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-05-11 14:10:19 +0300

    Update battletable.cpp

--- Sea_Battle_qt/battletable.cpp
+++ Sea_Battle_qt/battletable.cpp
@@ -6,11 +6,17 @@ BattleTable::BattleTable(QObject *parent) : QObject(parent)
 
 void BattleTable::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
+    /*
+     * Здесь мы просто посылаем сигнал о том, что был произведён выстрел, 
+     * и отправляем координаты выстрела в слот SendFire(), описанный в файле mainwindow.cpp, 
+     * для дальнейшей отправки на сервер.
+    */
    emit fire(event->pos().x() / 24, event->pos().y() / 24);
 }
 
 QRectF BattleTable::boundingRect() const
 {
+    //Формируем квадрат поля противника для вычисления координат.
     return QRectF(0, 0, 240, 240);
 }
 
