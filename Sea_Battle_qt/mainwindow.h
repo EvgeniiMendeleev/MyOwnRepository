394e92ac26836a85fe6f52c154d97b79d52b969b
SymplyAHuman <43094600+EvgeniiMendeleev@users.noreply.github.com>
2019-05-11 14:08:49 +0300

    Update mainwindow.h

--- Sea_Battle_qt/mainwindow.h
+++ Sea_Battle_qt/mainwindow.h
@@ -34,28 +34,36 @@ class MainWindow;
 
 enum ResultOfShot {not_hit,
                    hit,
-                   kill};
+                   kill};   //Результат выстрела
 
 enum Msg_type {result_of_shot,
                enemy_shot,
-               error};
+               state_for_client,
+               error};      //Тип сообщений, отправляемых с сервера клиенту.
 
 enum states {WaitingOfReadyPlayer,
-             WaitingOfConnection,
-             PlacingShips, Win, Lose};
+             WaitingOfConnection, 
+             Win, 
+             Lose};         //Состояния клиента, для отображения соответствующих сообщений на экране. 
 
-struct Shot
+struct Shot         //Выстрел, отправляемый на сервер.
 {
     int16_t PosX;
     int16_t PosY;
 };
 
-struct Message
+struct Message      //Сообщение, получаемое с сервера.
 {
     int16_t type;
     int16_t PosX;
     int16_t PosY;
     int16_t Result;
+};
+
+struct StateForClient   //Сообщение о состоянии клиента для вывода промежуточных окон.
+{
+    int16_t type;
+    int16_t state;
 };
 
 class MainWindow : public QMainWindow
@@ -65,22 +73,23 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-     void Preparing_for_Battle(); //Функция для отображения фрейма с нераставленными кораблями и полем.
-     void Main_Menu_off();        //Функция для скрытия кадра главного меню.
-     void Main_Menu_on();         //Функция для отображения кадра главного меню.
-     void BATTLE();               //Функция для отображения кадра с битвой.
+     void Preparing_for_Battle();                                //Функция для отображения фрейма с нераставленными кораблями и полем.
+     void Main_Menu_off();                                       //Функция для скрытия кадра главного меню.
+     void Main_Menu_on();                                        //Функция для отображения кадра главного меню.
+     void BATTLE();                                              //Функция для отображения кадра с битвой.
+     Message* convertToStruct(int16_t* buffer, Msg_type type);   //Функция для конвертации данных, отпраленных с сервера, в структуру для удобства работы.
 
 private slots:
     void on_Connection_clicked();                //Кнопка подключения в главном меню.
     void on_BattleButton_clicked();              //Кнопка готовности на этапе расстановки кораблей.
-    void SendFire(int16_t x, int16_t y);         //Отправка координат серверу.
-    void ReadFromServer();                       //Чтение данных с сервера.
+    void SendFire(int16_t x, int16_t y);         //Отправка координат выстрела серверу.
+    void ReadFromServer();                       //Чтение данных с сокета, на который пришли данные с сервера.
 
 private:
     Ui::MainWindow *ui;     //Главное меню.
     int ClientSocket;       //Сокет клиента, с помощью которого будет общение с сервером.
 
-    QTimer* MyTimer;        //Таймер, по истечению которого будут читаться данные с сокета во время боя.
+    QTimer* MyTimer;        //Таймер, по истечению которого будут читаться данные с сокета.
 
     //---Переменные для отображения какого - либо фрейма---
     QPixmap frame;
@@ -99,12 +108,20 @@ private:
     //i = 6-9 - однопалубники
 
     //----Переменные для работы с таблицей кораблей----
+    //--------(используется разделяемая память)--------
+    /*
+     * Используется разделяемая память из тех соображений, что
+     * не только данному процессу нужна информация о кораблях,
+     * чтобы потом отправить их на сервер, но и процессу, описанному в файле
+     * ship.cpp в функции mouseReleaseEvent(), чтобы выполнить проверку на размещение корабля
+     * в данные клетки. Также помогает избежать ошибок при приёме данных на сервер.
+    */
     int memID;
     int16_t* table;
     //-------------------------------------------------
 
-    BattleTable* Table;        //Поле, на котором вычисляются координаты выстрела для противника
-    states StateOfClient;      //Состояние клиента.
+    BattleTable* Table;        //Поле, на котором вычисляются координаты выстрела для противника.
+    states StateOfClient;      //Переменная для хранения состояния клиента.
 };
 
 #endif // MAINWINDOW_H
